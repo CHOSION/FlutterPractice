4c4a9146121a307309a0aa1654afaf78bd0ebb9f
CHOSION <chosion0107@gmail.com>
2022-04-07 16:18:47 +0900

    220407_1

---
+++ flutter_app_stateful/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
